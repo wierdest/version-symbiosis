80abe8f4fe814a586975329ee760457b7980eded
andre <andrlzpt@protonmail.com>
2025-01-19 10:15:36 -0300

    first commit to be able to build for local testing using vsce

---
+++ README.md
@@ -0,0 +1,32 @@
+# version-symbiosis README
+
+version-symbiosis links the version variable in package.json to other versioning files in your project.
+
+## Features
+
+* super simple
+* syncs one versioning variable per specified file  
+* never forget to update versioning variables in your project files again!
+
+## Requirements
+
+version-symbiosis does not require any special dependencies.
+
+## Extension Settings
+
+version-symbiosis contributes the following setting:
+
+* `versionSymbiosis.filesToUpdate`: An array of objects containing `targetFile` and `targetVariable` properties.
+
+## Known Issues
+
+Issues are yet to be known.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of version-symbiosis
+
+---
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,69 @@
+const vscode = require('vscode')
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {	
+	const packageJsonPattern = new vscode.RelativePattern(vscode.workspaceFolders[0], '**/package.json')
+	const watcher = vscode.workspace.createFileSystemWatcher(packageJsonPattern)
+	watcher.onDidChange(updateVersionInFile)
+	async function updateVersionInFile() {
+		const config = vscode.workspace.getConfiguration('versionSymbiosis')
+		const filesToUpdate = config.get('filesToUpdate')
+
+		if (!filesToUpdate || !Array.isArray(filesToUpdate) || filesToUpdate.length === 0) {
+			vscode.window.showWarningMessage(
+				'VersionSymbiosis: Please, configure "filesToUpdate" in settings as an array of objects with "targetFile" and "variableName" properties.')
+			return
+		}
+
+		try {
+			const [packageJsonUri] = await vscode.workspace.findFiles(packageJsonPattern)	
+			if (!packageJsonUri) {
+				vscode.window.showErrorMessage('VersionSymbiosis: package.json not found')
+				return
+			}
+
+			const packageJsonContent = await vscode.workspace.fs.readFile(packageJsonUri)
+			const packageJson = JSON.parse(packageJsonContent.toString())
+			const version = packageJson.version
+
+			if (!version) {
+				vscode.window.showErrorMessage('VersionSymbiosis: Version not found in package.json.')
+				return
+			}
+
+			for (const fileConfig of filesToUpdate) {
+				const { targetFile, variableName: targetVariable } = fileConfig
+				if (!targetFile || !targetVariable) {
+					vscode.window.showWarningMessage('VersionSymbiosis: Each entry in "filesToUpdate" must have "targetFile" and "targetVariable " properties.')
+					continue
+				}
+				
+				const targetFilePattern = new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], targetFile)
+				const [targetFileUri] = await vscode.workspace.findFiles(targetFilePattern)
+				if (!targetFileUri) {
+					vscode.window.showWarningMessage(`VersionSymbiosis: File not found: ${targetFile}.`)
+					continue
+				}
+
+				const targetFileContent = (await vscode.workspace.fs.readFile(targetFileUri)).toString()
+				const regex = new RegExp(`(${targetVariable}\s*=\s*['"])([\d.]+)(['"])`)
+				const updatedContent = targetFileContent.replace(regex, `$1${version}$3`)
+
+				await vscode.workspace.fs.writeFile(targetFileUri, Buffer.from(updatedContent, 'utf-8'))
+				vscode.window.showInformationMessage(`VersionSymbiosis: Updated version in ${targetFile}.`)
+			}
+		} catch (e) {
+			vscode.window.showErrorMessage(`VersionSymbiosis: Error: ${e.message}`)
+		}
+	}
+	context.subscriptions.push(watcher)
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
